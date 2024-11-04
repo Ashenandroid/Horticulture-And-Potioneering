8b373013c9d4ad8d642676e1237abe73d530a257
Andrew <arweaver@mtu.edu>
2024-11-04 14:08:52 -0500

    added textures for all available brews

--- src/main/java/ash/horticulturepotioneering/datagen/ModModelProvider.java
+++ src/main/java/ash/horticulturepotioneering/datagen/ModModelProvider.java
@@ -1,9 +1,11 @@
 package ash.horticulturepotioneering.datagen;
 
+import ash.horticulturepotioneering.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
@@ -17,6 +19,14 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-
+        // Brews
+        itemModelGenerator.register(ModItems.FIRE_RESISTANCE_BREW, Models.GENERATED);
+        itemModelGenerator.register(ModItems.HASTE_BREW, Models.GENERATED);
+        itemModelGenerator.register(ModItems.INVISIBILITY_BREW, Models.GENERATED);
+        itemModelGenerator.register(ModItems.NIGHT_VISION_BREW, Models.GENERATED);
+        itemModelGenerator.register(ModItems.REGENERATION_BREW, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SPEED_BREW, Models.GENERATED);
+        itemModelGenerator.register(ModItems.STRENGTH_BREW, Models.GENERATED);
+        itemModelGenerator.register(ModItems.WATER_BREATHING_BREW, Models.GENERATED);
     }
 }
